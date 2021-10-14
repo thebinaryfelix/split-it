73e20993de93788d889dfad8ed1229f5146bdc2a
Mateus Félix <mateus.cfoliveira@gmail.com>
2021-10-14 18:13:58 -0300

    refactor(person): delete unused methods

--- src/services/person.ts
+++ src/services/person.ts
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from 'uuid'
-import { ICreatePerson, IPerson, IUpdatePerson } from 'db/interfaces'
-import { getItem, setItem, workspace } from 'db/localStorage'
+import { ICreatePerson, IPerson } from 'db/interfaces'
+import { getItem, workspace } from 'db/localStorage'
 
 const personsPath = `${workspace}.persons`
 
@@ -11,29 +11,4 @@ export const createPerson = (person: ICreatePerson): IPerson => {
 export const getPersons = (): IPerson[] => {
   const { persons } = getItem(personsPath)
   return persons ?? []
-}
-
-export const deletePerson = (id: string): void => {
-  const persons = getPersons()
-  const personsCopy = [...persons]
-  const productToDeleteIndex = persons.findIndex(person => person.id === id)
-
-  personsCopy.splice(productToDeleteIndex, 1)
-
-  setItem(personsPath, { persons: [...personsCopy] })
-}
-
-export const updatePerson = (id: string, data: IUpdatePerson): void => {
-  const persons = getPersons()
-  const personsCopy = [...persons]
-  const personToUpdateIndex = persons.findIndex(product => product.id === id)
-
-  const originalPerson: IPerson = persons[personToUpdateIndex]
-
-  personsCopy.splice(personToUpdateIndex, 1, {
-    ...originalPerson,
-    ...data,
-  })
-
-  setItem(personsPath, { persons: [...personsCopy] })
 }
