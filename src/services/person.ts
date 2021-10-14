4ae03d508715e2225b5c4c3256f7421c97cc935a
Mateus Félix <mateus.cfoliveira@gmail.com>
2021-10-14 02:45:11 -0300

    feat(person): create service

---
+++ src/services/person.ts
@@ -0,0 +1,51 @@
+import { getItem, ICreatePerson, IPerson, IUpdatePerson, setItem, workspace } from 'db/localStorage'
+import { v4 as uuidv4 } from 'uuid'
+
+const personsPath = `${workspace}.persons`
+
+export const createPerson = (person: ICreatePerson): void => {
+  const newPerson: IPerson = { ...person, id: uuidv4(), amount: 0 }
+
+  const { persons } = getItem(personsPath)
+
+  if (!persons) {
+    setItem(personsPath, { persons: [newPerson] })
+  } else {
+    setItem(personsPath, { persons: [...persons, newPerson] })
+  }
+}
+
+export const getPersons = (): IPerson[] => {
+  const { persons } = getItem(personsPath)
+  return persons ?? []
+}
+
+export const getPerson = (id: string): IPerson | undefined => {
+  const persons = getPersons()
+  return persons.find(person => person.id === id)
+}
+
+export const deletePerson = (id: string): void => {
+  const persons = getPersons()
+  const personsCopy = [...persons]
+  const productToDeleteIndex = persons.findIndex(person => person.id === id)
+
+  personsCopy.splice(productToDeleteIndex, 1)
+
+  setItem(personsPath, { persons: [...personsCopy] })
+}
+
+export const updatePerson = (id: string, data: IUpdatePerson): void => {
+  const persons = getPersons()
+  const personsCopy = [...persons]
+  const personToUpdateIndex = persons.findIndex(product => product.id === id)
+
+  const originalPerson: IPerson = persons[personToUpdateIndex]
+
+  personsCopy.splice(personToUpdateIndex, 1, {
+    ...originalPerson,
+    ...data,
+  })
+
+  setItem(personsPath, { persons: [...personsCopy] })
+}
